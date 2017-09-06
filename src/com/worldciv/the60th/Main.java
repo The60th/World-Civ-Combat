6772975fa5971fe9602d622261dddf4b1ba8bc33
JustinFTC8397 <justinthe60th@gmail.com>
2017-09-06 18:31:48 -0400

    Main class template

--- src/com/worldciv/the60th/Main.java
+++ src/com/worldciv/the60th/Main.java
@@ -1,8 +1,64 @@
 package com.worldciv.the60th;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin implements Listener{
+    FileConfiguration config = getConfig();
+    private static Plugin plugin;
+    //public static ScoreboardManager manager = Bukkit.getScoreboardManager();
+    //public static Scoreboard board = manager.getNewScoreboard();
+    //public static Team team = board.registerNewTeam("holdingLight");
+    public void onEnable() {
+        PluginDescriptionFile pdfFile = this.getDescription();
+        Logger logger = Logger.getLogger("Minecraft");
+
+        logger.info(pdfFile.getName()
+                + "has successfully enabled. The current version is: "
+                + pdfFile.getVersion());
+        registerEvents();
+        //team.setPrefix("Torch");
+        //System.out.print(team.getName());
+        // System.out.print(team.getPrefix());
+
+    }
+
+    public void onDisable() {
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = Logger.getLogger("Minecraft");
+        logger.info(pdfFile.getName() + "has successfully disabled.");
+    }
+
+    public void registerCommands(){
+        //this.getCommand("name").setExecutor(new class());
+    }
+    public void registerEvents(){
+        PluginManager pm = getServer().getPluginManager();
+       // pm.registerEvents(new class(), this);
+        //pm.registerEvents(new class(), this);
+    }
+    private void registerPermissons(){
+        //PluginManager pm = getServer().getPluginManager();
+        //Permission p = new Permission("Permisson name");
+        //pm.addPermission(p);
+
+    }
+
+
+
+    public static Plugin getPlugin() {
+        return plugin;
     }
 }
